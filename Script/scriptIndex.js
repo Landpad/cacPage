05a6c694422578b1254b03e984593fcc6bfdce14
landpad <20844115+Landpad@users.noreply.github.com>
2024-06-13 01:39:02 -0300

    Finished best movies carousel

--- Script/scriptIndex.js
+++ Script/scriptIndex.js
@@ -1,17 +1,21 @@
+//API
+
 const ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTNmMDNiMjJlMWU4MjcxOTUwZWRlODllNGYyZmZlMiIsInN1YiI6IjY2NjYzNzJhOTE0Yjg4OTA3YWU5YmJlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RQS4QslLRYDtk4jt5ryBU9FvWlsSxoIkyLrA5g5fzPY';
 const API_KEY = 'fe3f03b22e1e8271950ede89e4f2ffe2';
-const API_URL = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`;
+const trendingWeekURL = `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`;
+const trendingDayURL = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
 const moviesContainer = document.getElementById("movies");
+const bestMoviesContainer = document.getElementById("bestMovies");
 
 
-async function fetchMovies(){
+async function fetchMovies(URL, containerId){
     try{
-        const response = await fetch(API_URL);
+        const response = await fetch(URL);
         const data = await response.json();
 
         data.results.forEach(media => {
             const movieCard = createMovieCard(media);
-            moviesContainer.appendChild(movieCard);
+            containerId.appendChild(movieCard);
         });
     }   catch(error){
             console.log('Error fetching data: ', error);
@@ -25,21 +29,31 @@ function createMovieCard(media){
     movieCard.classList.add("movieItem");
 
     movieCard.innerHTML = `
-        <img src="https://image.tmdb.org/t/p/w500/${backdrop_path}.jpg" class="movieImgRounded">
+        <div class="img" draggable="false">
+            <img src="https://image.tmdb.org/t/p/w500/${backdrop_path}.jpg" class="movieImgRounded" draggable="false">
+        </div>
         <div class="movieTitle">
             <h4>${title || name}</h4>
-            </div>
+        </div>
         `;
         return movieCard;
 }
 
-fetchMovies();
+fetchMovies(trendingWeekURL, moviesContainer);
+fetchMovies(trendingDayURL, bestMoviesContainer);
 
-const movieItemDiv = document.querySelector("#movies");
+// Carousel Logic
 
-movieItemDiv.addEventListener('mouseover',(event) =>{
-    let movieDiv = document.querySelector(".movieTitle");
-    movieDiv.style.zIndex = "1";
+const moviesCarousel = document.querySelector("#bestMovies");
+const arrowButtons = document.querySelectorAll(".arrowContainer");
+const moviesCarouselChildrens = [...moviesCarousel.children];
+
+let cardPerView = 660;
+
+arrowButtons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        moviesCarousel.scrollLeft += btn.id === "left" ? -660:660;
+        console.log(btn.id);
+    });
 });
-
 
